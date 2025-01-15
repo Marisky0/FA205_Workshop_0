dc5026229ca1409b452c79ed85379e7a93dc450e
Marisky <mariskalebamoff@gmail.com>
2025-01-16 12:02:24 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+let x = 0
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  fill(70, 200, 10, 70);
+  circle(mouseX, mouseY, 40, 60)
+
+  x = x + 3
 }
